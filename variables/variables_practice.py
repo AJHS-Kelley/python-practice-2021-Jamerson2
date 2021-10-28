350d1d6473f9912dcc40a9bc7a97bb860704ec9f
Jamerson f <90413446+Jamerson2@users.noreply.github.com>
2021-10-28 14:10:16 -0400

    Update variables_practice.py

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,1 +1,11 @@
  # Variables Practice, Ryan Kelley, 10/28/21 1:46pm, Version 0.0
+
+ # common Data Types and Variables in python
+
+ life_count = 3 # Integer value, all whole numbers, +/- and 0.
+ account_balance = 4232.57 # Floating Point value, +/-.
+ User_name = "Billy Boe Joe" # String, a collection of letters/numbers.
+
+ print(life_count)
+ print(account_balance)
+ print(User_name)
